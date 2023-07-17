6018e37c2684a6298e654dadb148b349080d0c04
Pär Tojkander <par.tojkander@gmail.com>
2023-07-17 13:36:59 +0200

    Add method for getting row and column

--- binero_solver.py
+++ binero_solver.py
@@ -7,9 +7,17 @@ class BineroSolver:
 
     def print_board(self):
         for row in self.board:
-            print(''.join([str(i) for i in row]))
+            print(''.join([str(cell) for cell in row]))
+
+    def get_row(self, index):
+        return ''.join([str(cell) for cell in self.board[index]])
+
+    def get_column(self, index):
+        column = [row[index] for row in self.board]
+        return ''.join([str(cell) for cell in column])
 
 
 if __name__ == '__main__':
     b = BineroSolver()
-    b.print_board()
+    print(b.get_row(1))
+    print(b.get_column(3))
