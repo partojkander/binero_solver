bf8d8d89216dab9b5c109db2417fe5ffcb761bc5
Pär Tojkander <par.tojkander@gmail.com>
2023-07-17 14:25:27 +0200

    Add method to validate board

--- binero_solver.py
+++ binero_solver.py
@@ -1,7 +1,10 @@
+from typing import List
+
+
 class BineroSolver:
 
-    board = ['0010',
-             '0001',
+    board = ['0110',
+             '1100',
              '1001',
              '0110']
 
@@ -14,8 +17,16 @@ class BineroSolver:
     def get_column(self, index) -> str:
         return ''.join([row[index] for row in self.board])
 
-    @staticmethod
-    def is_valid_sequence(sequence: str) -> bool:
+    def get_rows(self) -> List[str]:
+        return self.board
+
+    def get_columns(self) -> List[str]:
+        columns = []
+        for index in range(len(self.board[0])):
+            columns.append(''.join([row[index] for row in self.board]))
+        return columns
+
+    def is_valid_sequence(self, sequence: str) -> bool:
         print(f"Validating sequence '{sequence}'")
         if '000' in sequence:
             print(f"Err: sequence '{sequence}' contains '000'")
@@ -32,7 +43,19 @@ class BineroSolver:
 
         return True
 
+    def is_board_solved(self):
+        print("Validating rows...")
+        for row in self.get_rows():
+            if not self.is_valid_sequence(row):
+                return False
+        print("Validating columns...")
+        for column in self.get_columns():
+            if not self.is_valid_sequence(column):
+                return False
+
+        return True
+
 
 if __name__ == '__main__':
     b = BineroSolver()
-    print(b.is_valid_sequence(b.get_row(1)))
+    print(b.is_board_solved())
