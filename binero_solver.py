b8d6bd0ae9365e452e4d2c69de46f92d644a17cc
Pär Tojkander <par.tojkander@gmail.com>
2023-07-17 14:10:45 +0200

    Refactor board to be list of strings + add validation

--- binero_solver.py
+++ binero_solver.py
@@ -1,23 +1,38 @@
 class BineroSolver:
 
-    board = [[0, 1, 1, 0],
-             [1, 0, 0, 1],
-             [1, 0, 0, 1],
-             [0, 1, 1, 0]]
+    board = ['0010',
+             '0001',
+             '1001',
+             '0110']
 
     def print_board(self):
-        for row in self.board:
-            print(''.join([str(cell) for cell in row]))
+        print('\n'.join([row for row in self.board]))
 
-    def get_row(self, index):
-        return ''.join([str(cell) for cell in self.board[index]])
+    def get_row(self, index) -> str:
+        return self.board[index]
 
-    def get_column(self, index):
-        column = [row[index] for row in self.board]
-        return ''.join([str(cell) for cell in column])
+    def get_column(self, index) -> str:
+        return ''.join([row[index] for row in self.board])
+
+    @staticmethod
+    def is_valid_sequence(sequence: str) -> bool:
+        print(f"Validating sequence '{sequence}'")
+        if '000' in sequence:
+            print(f"Err: sequence '{sequence}' contains '000'")
+            return False
+        if '111' in sequence:
+            print(f"Err: sequence '{sequence}' contains '111'")
+            return False
+        if ' ' in sequence:
+            print(f"Err: sequence '{sequence}' contains empty cell(s)")
+            return False
+        if sequence.count('0') != sequence.count('1'):
+            print(f"Err: sequence '{sequence}' has different amount of 1's and 0's")
+            return False
+
+        return True
 
 
 if __name__ == '__main__':
     b = BineroSolver()
-    print(b.get_row(1))
-    print(b.get_column(3))
+    print(b.is_valid_sequence(b.get_row(1)))
