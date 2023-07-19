089b1d201a49410be0aa8d2f0b39e9a218606afa
Pär Tojkander <par.tojkander@gmail.com>
2023-07-19 13:01:41 +0200

    Add first iteration of the solver

---
+++ binero_solver.py
@@ -0,0 +1,39 @@
+from copy import copy
+
+from binero_board import BineroBoard
+
+
+class BineroSolver:
+
+    board = BineroBoard()
+    board.board = [' 1 1',
+                   '00 0',
+                   '0010',
+                   ' 101']
+
+    def fill_around_double(self, sequence: str) -> str:
+        sequence = sequence.replace(' 00', '100')
+        sequence = sequence.replace('00 ', '001')
+        sequence = sequence.replace(' 11', '011')
+        sequence = sequence.replace('11 ', '110')
+        return sequence
+
+    def fill_gap(self, sequence: str) -> str:
+        sequence = sequence.replace('0 0', '010')
+        sequence = sequence.replace('1 1', '101')
+        return sequence
+
+    def apply_solutions(self):
+        while True:
+            old_board = copy(self.board.board)
+            for row_index, row in enumerate(self.board.get_rows()):
+                row = self.fill_around_double(row)
+                row = self.fill_gap(row)
+                self.board.set_row(row_index, row)
+            for column_index, column in enumerate(self.board.get_columns()):
+                column = self.fill_around_double(column)
+                column = self.fill_gap(column)
+                self.board.set_column(column_index, column)
+            if self.board.board == old_board:
+                print("No more solutions available, exiting...")
+                break
